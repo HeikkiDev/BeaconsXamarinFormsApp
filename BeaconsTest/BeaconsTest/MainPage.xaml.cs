c53521f41e4862643195d75a35f6127e4e5e0f38
Enrique <enri46@gmail.com>
2018-05-12 21:09:36 +0200

    Ejemplo de uso del servicio de Beacons en la MainPage.

--- BeaconsTest/BeaconsTest/MainPage.xaml.cs
+++ BeaconsTest/BeaconsTest/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeaconsTest.Services.Beacons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,55 @@ namespace BeaconsTest
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        ListView _list;
+
+        public event EventHandler ListChanged;
+        public List<SharedBeacon> Data { get; set; }
+
+        public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+
+            //
+
+            Data = new List<SharedBeacon>();
+
+            var beaconService = DependencyService.Get<IBeaconMonitoringService>();
+            beaconService.ListChanged += BeaconService_ListChanged;
+            beaconService.DataClearing += BeaconService_DataClearing;
+
+            beaconService.InitializeService();
+            //
+
+            BackgroundColor = Color.White;
+            Title = "AltBeacon Forms Sample";
+            //Content = BuildContent();
+        }
+
+        private View BuildContent()
+        {
+            _list = new ListView
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                ItemTemplate = new DataTemplate(typeof(ListItemView)),
+                RowHeight = 90,
+            };
+
+            _list.SetBinding(ListView.ItemsSourceProperty, "Data");
+
+            return _list;
+        }
+
+        private void BeaconService_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            // Data será una Bindable property con la lista de Beacons! :D
+            Data = e.Data;
+        }
+
+        private void BeaconService_DataClearing(object sender, EventArgs e)
+        {
+            // Informar al usuario de que no hay Beacons disponibles y limpiar la UI
+            Data.Clear();
+        }
+    }
 }
